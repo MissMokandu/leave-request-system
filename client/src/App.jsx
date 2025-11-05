cd5a764c67533f36af20d0487c5bd9a817e558ff
Benadette Mokandu <benadettemokandu@gmail.com>
2025-11-05 19:40:05 +0300

    app, main.jsx set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import AdminDashboard from './pages/AdminDashboard';
+import EmployeeDashboard from './pages/EmployeeDashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        
+        <Route
+          path="/employee-dashboard"
+          element={
+            <ProtectedRoute requiredRole="employee">
+              <EmployeeDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin-dashboard"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,11 @@
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // This should point to your actual CSS file
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
